0eedc1036e63b57f524ceb4524a798b56964f0fd
Akshat9080 <81543625+Akshat9080@users.noreply.github.com>
2022-08-08 11:50:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,34 @@ $(document).ready(function(){
         $('.navbar').removeClass("sticky");
     }
   })
+  
+  var typed = new Typed(".typing",{
+    strings: ["Programmer","Engineer","Frontend-developer","Designer"],
+    typespeed:100,
+    backspeed:80,
+    loop:true
+  });
+
+  $(`.carousel`).owlCarousel({
+    margin: 20,
+    loop:true,
+    autoplayTimeOut:2000,
+    autoplayHoverPause : true,
+    responsive: {
+      0:{
+        items:1,
+        nav:false
+      },
+      600:{
+        items:2,
+        nav:false
+      },
+      1000:{
+        items:3,
+        nav:false
+      }
+       
+    }
+  });
 });
 
-var typed = new Typed(".typing",{
-  strings: ["Programmer","Engineer","Frontend-developer","Designer"],
-  typespeed:100,
-  backspeed:60,
-  loop:true
-});
